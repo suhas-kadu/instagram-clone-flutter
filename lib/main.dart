8059a5fe099ca9edddbc4dbd3317a66ada7704ba
suhas-kadu <kadusuhas72@gmail.com>
2022-03-05 02:12:35 +0530

    firebase app initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
 import 'package:instagram_clone_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyC1NLRpA3gccePcLs1oZ499frrIExPeHr4",
+          authDomain: "instagram-clone-flutter-1edb6.firebaseapp.com",
+          projectId: "instagram-clone-flutter-1edb6",
+          storageBucket: "instagram-clone-flutter-1edb6.appspot.com",
+          messagingSenderId: "889741998736",
+          appId: "1:889741998736:web:b08571ab5b403c5e3f3615"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -15,6 +34,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Instagram',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
       home: ResponsiveLayout(
           webScreenLayout: WebScreenLayout(),
           mobileScreenLayout: MobileScreenLayout()),
